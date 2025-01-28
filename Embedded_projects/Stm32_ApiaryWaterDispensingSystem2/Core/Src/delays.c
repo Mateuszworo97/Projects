9ea33fba3d962a9b34f9b58ebdd137adde6357d3
Mateusz Worotnicki <mateusz.worotnicki@gmail.com>
2025-01-28 20:23:26 +0100

    -added repair files

---
+++ Embedded_projects/Stm32_ApiaryWaterDispensingSystem2/Core/Src/delays.c
@@ -0,0 +1,23 @@
+/*
+ * delays.c
+ *
+ *  Created on: 24 kwi 2023
+ *      Author: Y700
+ */
+
+#ifndef SRC_DELAYS_C_
+#define SRC_DELAYS_C_
+
+#include "main.h"
+#include "stm32f4xx_hal.h"
+//#include "tim.h"
+
+#include "delays.h"
+
+//void Delay_us(uint16_t us)
+//{
+//	htim3.Instance->CNT = 0;
+//	while(htim3.Instance->CNT <= us);
+//}
+
+#endif /* SRC_DELAYS_C_ */
